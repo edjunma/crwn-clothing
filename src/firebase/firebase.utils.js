583dcd88da1aaf68a2a66005c0d62a9122b1f78d
edjunma <junwenma@buffalo.edu>
2019-09-08 00:00:20 -0400

    moving shop data to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,40 +3,43 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+	apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+	authDomain: 'crwn-db.firebaseapp.com',
+	databaseURL: 'https://crwn-db.firebaseio.com',
+	projectId: 'crwn-db',
+	storageBucket: 'crwn-db.appspot.com',
+	messagingSenderId: '850995411664',
+	appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
+	if (!userAuth) return;
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
 
-  const snapShot = await userRef.get();
+	return userRef;
+};
 
-  if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData
-      });
-    } catch (error) {
-      console.log('error creating user', error.message);
-    }
-  }
-
-  return userRef;
+export const addCollectionAndDocuments = (collectionKey, objectsToAdd) => {
+	const collectionRef = firestore.collection(collectionKey);
+	console.log(collectionRef);
 };
 
 export const auth = firebase.auth();
