c98492f31ac04b2fde8e28476a05b2bd78b41f05
edjunma <edjunma@gmail.com>
2019-11-06 00:27:14 -0500

    adding snapshot component for collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,8 +4,19 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
+import { firestore } from '../../firebase/firebase.utils';
+
 class ShopPage extends React.Component {
+	unsubscribeFromSnapshot = null;
+
+	componentDidMount() {
+		const collectionRef = firestore.collection('collections');
+
+		collectionRef.onSnapshot();
+	}
+
 	render() {
+		const { match } = this.props;
 		return (
 			<div className='shop-page'>
 				<Route exact path={`${match.path}`} component={CollectionsOverview} />
